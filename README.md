7f446dad2e950c1887fc3f00ca99b97f6c793fcb
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-22 17:06:21 -0700

    added html routes, api routes, and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# note-taker
+# NOTE TAKER
+
+## DESCRIPTION
+This app was developed for a homework assignment in the UW Bootcamp Course. It's a basic note taking app that allows you to save a note with a title and contents.
+
+## LINK
+TBD
+
+## SCREENSHOT
+![note taker screenshot](public/assets/img/note-taker-screenshot.png)
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-const readFilePromise = util.promisify(fs.readfile);
-const writeFilePromise = util.promisify(fs.writefile);
+
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,25 +14,50 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
 
 const readFile = async () => {
-    readFilePromise("./db/db.json", "utf8")
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+    let data = await readFilePromise("./db/db.json", "utf8")
+    return JSON.parse(data)
 }
 
-const writeFile = (data) => {
+const writeFile = async (data) => {
     data = JSON.stringify(data, null, 2)
-    writeFilePromise("./db/.db.json", data)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+    await writeFilePromise("./db/db.json", data)
 }
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
+app.get('/api/notes', async (req, res) => {
+    let data = await readFile()
+    res.json(data)
+});
+
+app.get('/api/notes/:id', async (req, res) => {
+    const note = req.params.id;
+    let data = await readFile()
+    let [matchedId] = data.filter(id => id.title === note )
+    if (matchedId) return res.json(matchedId);
+    return res.json(false);
+});
+
+app.post('/api/notes', async (req, res) => {
+    const newNote = req.body;
+    let data = await readFile()
+    await writeFile([...data, newNote])
+    res.json(newNote);
+});
+
+app.delete('/api/notes/:id', async (req, res) => {
+    const note = req.params.id;
+    let data = await readFile()
+    let [matchedId] = data.filter(id => id.title === note)
+    if (matchedId) return res.json(matchedId);
+    return res.json(false);
+});
 
 
 
